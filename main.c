5eba5ef08aaf16f16ea905e5c9e904f5e86002ec
一只枫兔 <114807153+Schuke-Shu@users.noreply.github.com>
2023-08-02 13:48:38 +0800

    2023/08/02

---
+++ main.c
@@ -0,0 +1,44 @@
+//
+// Created by schuke on 2023/8/2.
+//
+#include <stdio.h>
+
+void base();
+void pointer();
+
+int main()
+{
+//    base();
+    pointer();
+    return 0;
+}
+
+// 基础
+void base()
+{
+    int a = 1; // 声明变量
+    printf("%d", a);
+    int b = 0;
+    printf("%d", b);
+
+    // 单行注释
+    /*
+     * 多行注释
+     * 多行注释
+     */
+    // 注释内容会被编译器忽略
+}
+
+// 指针
+void pointer()
+{
+    int i = 1;
+    int* p = &i; // '&'为取地址符号
+    printf("number i: %d, pointer: %p, pointer address: %p\n", i, p, &p);
+
+    printf("pointer: %p, value pointed by the pointer: %d, the pointer that point to pointer: %p\n", p, *p, &p);
+
+    *p = 10;
+    // 改变指针指向的值，原来的变量i也被改变
+    printf("pointer: %p, value pointed by the pointer: %d, the pointer that point to pointer: %p, number i: %d\n", p, *p, &p, i);
+}
