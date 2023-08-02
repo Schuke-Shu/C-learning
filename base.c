3cdf93fdb88ff5884ccf1e7ed753a4776ac6660a
一只枫兔 <114807153+Schuke-Shu@users.noreply.github.com>
2023-08-02 11:54:28 +0800

    Initial commit

---
+++ base.c
@@ -0,0 +1,17 @@
+#include <stdio.h> // 引入头文件
+
+int main(int argc,char *argv[])
+{
+    int a = 1; // 声明变量
+    printf("%d", a);
+    int b = 0;
+    printf("%d", b);
+
+    return -1;
+    // 单行注释
+    /*
+     * 多行注释
+     * 多行注释
+     */
+    // 注释内容会被编译器忽略
+}
