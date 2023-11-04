0a07496bf2477e4751929ceb5c1518ec62e5abf8
schuke <xk233qwq@163.com>
2023-11-04 13:10:09 +0800

    2023/11/04

---
+++ code/basic/data_type.c
@@ -0,0 +1,29 @@
+// 数据类型
+#include <stdio.h>
+#include <inttypes.h> // 支持可移植类型
+
+int main()
+{
+    short s;
+    int i;
+    long int l;
+    long long int ll;
+    char c;
+    float f;
+    double d;
+    double _Complex cp;
+//    double _Imaginary ig;
+
+    int32_t i32;
+
+    printf("Size of short: %lld\n", sizeof(short));
+    printf("Size of int: %lld\n", sizeof(int));
+    printf("Size of long: %lld\n", sizeof(long));
+    printf("Size of long long: %lld\n", sizeof(long long));
+    printf("Size of char: %lld\n", sizeof(char));
+    printf("Size of float: %lld\n", sizeof(float));
+    printf("Size of double: %lld\n", sizeof(double));
+    printf("Size of double _Complex: %lld\n", sizeof(double _Complex));
+//    printf("Size of double _Imaginary: %lld\n", sizeof(double _Imaginary));
+    printf("Size of int32_t: %lld\n", sizeof(int32_t));
+}
